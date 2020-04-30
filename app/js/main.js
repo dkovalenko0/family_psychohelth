c1e58f956176452d85042ce1f5d306505d482f18
dkovalenko0 <kovalenkodima715@gmail.com>
2020-05-01 01:12:20 +0300

    create reviews block

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
-// Your main script
 
-const index = 'This is a placeholder!';
+$('.slider').slick({
+	dots: true,
+	infinite: true,
+  	slidesToShow: 1,
+  	slidesToScroll: 1,
+	nextArrow: `<img src="assets/images/slider-arrow.svg" class="arrows next-arrow"></img>`,
+	prevArrow: `<img src="assets/images/slider-arrow.svg" class="arrows prev-arrow"></img>`,
+});
