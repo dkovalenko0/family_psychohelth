0dcc80dff15867c4dea0a77c2c722b073a14f539
dkovalenko0 <dimakovalenko715@gmail.com>
2020-07-01 19:54:39 +0300

    create adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,17 @@
+!(function ($) {
+  "use strict";
 
-$('.slider').slick({
-	dots: true,
-	infinite: true,
-  	slidesToShow: 1,
-  	slidesToScroll: 1,
-	nextArrow: `<img src="assets/images/slider-arrow.svg" class="arrows next-arrow"></img>`,
-	prevArrow: `<img src="assets/images/slider-arrow.svg" class="arrows prev-arrow"></img>`,
-});
+  $(".slider").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: `<img src="assets/images/slider-arrow.svg" class="arrows next-arrow"></img>`,
+    prevArrow: `<img src="assets/images/slider-arrow.svg" class="arrows prev-arrow"></img>`,
+  });
+
+  $(".nav-toogler").click(function (event) {
+    $(".nav-toogler, .nav-menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+})(jQuery);
--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,47 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat');
-const terser = require('gulp-terser');
-const rename = require('gulp-rename');
-const del = require('del');
-const browserSync = require('browser-sync').create();
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const replace = require('gulp-replace');
-const imagemin = require('gulp-imagemin');
-const plumber = require('gulp-plumber');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const babel = require("gulp-babel");
+const sourcemaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
+const terser = require("gulp-terser");
+const rename = require("gulp-rename");
+const del = require("del");
+const browserSync = require("browser-sync").create();
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const replace = require("gulp-replace");
+const imagemin = require("gulp-imagemin");
+const plumber = require("gulp-plumber");
 
 const paths = {
   html: {
-    src: './app/**/*.html',
-    dest: './build'
+    src: "./app/**/*.html",
+    dest: "./build",
   },
   styles: {
-    src: './app/scss/**/*.scss',
-    dest: './build/assets/css'
+    src: "./app/scss/**/*.scss",
+    dest: "./build/assets/css",
   },
   scripts: {
-    src: './app/js/**/*.js',
-    dest: './build/assets/js'
+    src: "./app/js/**/*.js",
+    dest: "./build/assets/js",
   },
   vendors: {
-    src: './app/js/vendors/**/*.js',
-    dest: './build/assets/js'
+    src: "./app/js/vendors/**/*.js",
+    dest: "./build/assets/js",
   },
   images: {
-    src: './app/images/**/*',
-    dest: './build/assets/images'
+    src: "./app/images/**/*",
+    dest: "./build/assets/images",
   },
   favicon: {
-    src: './app/favicon.ico',
-    dest: './build'
-  }
+    src: "./app/favicon.ico",
+    dest: "./build",
+  },
 };
 
-const clean = () => del(['./build']);
+const clean = () => del(["./build"]);
 
 // Cache busting to prevent browser caching issues
 const curTime = new Date().getTime();
@@ -49,16 +49,12 @@ const cacheBust = () =>
   gulp
     .src(paths.html.src)
     .pipe(plumber())
-    .pipe(replace(/cb=\d+/g, 'cb=' + curTime))
+    .pipe(replace(/cb=\d+/g, "cb=" + curTime))
     .pipe(gulp.dest(paths.html.dest));
-
 
 // Copies all html files
-const html =() =>
-  gulp
-    .src(paths.html.src)
-    .pipe(plumber())
-    .pipe(gulp.dest(paths.html.dest));
+const html = () =>
+  gulp.src(paths.html.src).pipe(plumber()).pipe(gulp.dest(paths.html.dest));
 
 // Convert scss to css, auto-prefix and rename into styles.min.css
 const styles = () =>
@@ -66,15 +62,15 @@ const styles = () =>
     .src(paths.styles.src)
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(
       rename({
-        basename: 'styles',
-        suffix: '.min'
+        basename: "styles",
+        suffix: ".min",
       })
     )
-    .pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(paths.styles.dest))
     .pipe(browserSync.stream());
 
@@ -84,14 +80,14 @@ const scripts = () =>
     .src(paths.scripts.src)
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(
-      babel({
-        presets: ['@babel/preset-env']
-      })
-    )
+    // .pipe(
+    //   babel({
+    //     presets: ['@babel/preset-env']
+    //   })
+    // )
     .pipe(terser())
-    .pipe(concat('app.min.js'))
-    .pipe(sourcemaps.write('.'))
+    .pipe(concat("app.min.js"))
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(paths.scripts.dest));
 
 // Minify all javascript vendors/libs and concat them into a single vendors.min.js
@@ -100,14 +96,14 @@ const vendors = () =>
     .src(paths.vendors.src)
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(
-      babel({
-        presets: ['@babel/preset-env']
-      })
-    )
+    // .pipe(
+    //   babel({
+    //     presets: ['@babel/preset-env']
+    //   })
+    // )
     .pipe(terser())
-    .pipe(concat('vendors.min.js'))
-    .pipe(sourcemaps.write('.'))
+    .pipe(concat("vendors.min.js"))
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(paths.vendors.dest));
 
 // Copy and minify images
@@ -129,17 +125,17 @@ const favicon = () =>
 function watchFiles() {
   browserSync.init({
     server: {
-      baseDir: './build'
+      baseDir: "./build",
     },
-    notify: false
+    notify: false,
   });
 
   gulp.watch(paths.styles.src, styles);
-  gulp.watch(paths.vendors.src, vendors).on('change', browserSync.reload);
-  gulp.watch(paths.favicon.src, favicon).on('change', browserSync.reload);
-  gulp.watch(paths.scripts.src, scripts).on('change', browserSync.reload);
-  gulp.watch(paths.images.src, images).on('change', browserSync.reload);
-  gulp.watch('./app/*.html', html).on('change', browserSync.reload);
+  gulp.watch(paths.vendors.src, vendors).on("change", browserSync.reload);
+  gulp.watch(paths.favicon.src, favicon).on("change", browserSync.reload);
+  gulp.watch(paths.scripts.src, scripts).on("change", browserSync.reload);
+  gulp.watch(paths.images.src, images).on("change", browserSync.reload);
+  gulp.watch("./app/*.html", html).on("change", browserSync.reload);
 }
 
 const build = gulp.series(
